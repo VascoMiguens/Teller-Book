ee4643d267304b158ced323bbb20b0d504072cee
Vasco <v_miguens@hotmail.com>
2023-07-05 17:13:47 +0100

    - added front-side and back-side to front and back covers - added cloth covering style to front cover and back cover

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,14 @@ export default function Home() {
   return (
     <div className='book-container'>
       <div className="book">
-        <div className="back"></div>
-        <div className="front"></div>
+        <div className="back">
+          <div className="front-side"></div>
+          <div className="back-side"></div>
+        </div>
+        <div className="front">
+          <div className="front-side"></div>
+          <div className="back-side"></div>
+        </div>
       </div>
     </div>
   )
