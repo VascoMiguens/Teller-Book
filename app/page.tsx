43156fba509dad7cf271823f3c058f89b0f6ae24
Vasco <v_miguens@hotmail.com>
2023-07-09 16:53:48 +0100

    .page flipp: -- state variable for pageRotations, stores the rotation angle for each of the pages in the array -- modified bookRight to slide at 5% of the scrollable area -- flippingPage -> current flipping page -- flipProgress -> calculation of flip progress -- newRotation -> caclulation the rotation of the curerntly flipping page

--- app/page.tsx
+++ app/page.tsx
@@ -9,39 +9,56 @@ export default function Home() {
   const bookRef = useRef<HTMLDivElement | null>(null);
   // State for the book's right property
   const [bookRight, setBookRight] = useState(0);
-
+  // State for page rotations
+  const [pageRotations, setPageRotations] = useState(new Array(12).fill(0));
 
   useEffect(() => {
     const handleScroll = () => {
       if (bookContainerRef.current) {
-        //Get the vertical scroll position
+        // Get the vertical scroll position
         const scrollY = window.scrollY || window.pageYOffset;
         // Get the total scrollable height (document height - window height)
         const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
         // Calculate scroll percentage (how much of the scrollable area has been scrolled)
-        const scrollPercentage = scrollY / scrollableHeight;
-        // Get the width of the book
+        const bookSlide = scrollY / (scrollableHeight * 0.05);
+
+        // For book sliding
         const bookRect = bookContainerRef.current.children[0].children[0].getBoundingClientRect();
-        // Calculate the width of the book as a percentage of the window width
         const bookWidth = bookRect ? (bookRect.width / window.innerWidth) * 100 : 0;
-        // Calculate the maximum right value
         const maxRight = Math.max(0, 25 - bookWidth);
-        // Calculate the new right value
-        const newBookRight = Math.min(maxRight, scrollPercentage * maxRight);
-  
+        const newBookRight = Math.min(maxRight, bookSlide * maxRight);
+
+        // For page flipping
+        //divide by 1 extra page so that initial page doesnt return to initial position, fix glitch
+        // number od pages
+        const totalPages = 12;
+        // the amount of scroll needed to flip a page (height of the container / number of pages)
+        const scrollPerFlip = scrollableHeight / totalPages;
+        // calculate which page is currently flipping
+        const flippingPage = Math.min(Math.floor(scrollY / scrollPerFlip), totalPages - 1);
+        // progress of the page that is currently being flipped
+        const flipProgress = (scrollY % scrollPerFlip) / scrollPerFlip;
+        // calculate the rotation of the page that is currently being flipped
+        const newRotation = flipProgress * (180 - flippingPage); // Subtract page index from 180 to have a layer effect
+        
+
         // Update the book's right property
         setBookRight(newBookRight);
+        // Update the page rotations
+        // if the page is not currently being flipped, set its rotation to 0, otherwise set its rotation to the new rotation,
+        //  so that the next page will be flipped only when the current page is completely flipped
+        setPageRotations(prev => prev.map((rotation, index) =>
+          index === flippingPage ? newRotation : (index < flippingPage ? 180 - (index /10) : 0)
+        ));
       }
     };
-  
+
     window.addEventListener('scroll', handleScroll);
-  
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  
   }, []);
-  
 
   return (
     <div className='book-container' ref={bookContainerRef}>
@@ -52,17 +69,17 @@ export default function Home() {
               <div className="front-side"></div>
               <div className="back-side"></div>
             </div>
-            <div className="page10"></div>
-            <div className="page9"></div>
-            <div className="page8"></div>
-            <div className="page7"></div>
-            <div className="page6"></div>
-            <div className="page5"></div>
-            <div className="page4"></div>
-            <div className="page3"></div>
-            <div className="page2"></div>
-            <div className="page1"></div>
-            <div className="front">
+            <div className="page10" style={{transform: `rotateY(${-pageRotations[10]}deg)`}}></div>
+            <div className="page9" style={{transform: `rotateY(${-pageRotations[9]}deg)`}}></div>
+            <div className="page8" style={{transform: `rotateY(${-pageRotations[8]}deg)`}}></div>
+            <div className="page7" style={{transform: `rotateY(${-pageRotations[7]}deg)`}}></div>
+            <div className="page6" style={{transform: `rotateY(${-pageRotations[6]}deg)`}}></div>
+            <div className="page5" style={{transform: `rotateY(${-pageRotations[5]}deg)`}}></div>
+            <div className="page4" style={{transform: `rotateY(${-pageRotations[4]}deg)`}}></div>
+            <div className="page3" style={{transform: `rotateY(${-pageRotations[3]}deg)`}}></div>
+            <div className="page2" style={{transform: `rotateY(${-pageRotations[2]}deg)`}}></div>
+            <div className="page1"style={{transform: `rotateY(${-pageRotations[1]}deg)`}}></div>
+            <div className="front" style={{transform: `rotateY(${-pageRotations[0]}deg)`}}>
               <div className="front-side"></div>
               <div className="back-side"></div>
             </div>
