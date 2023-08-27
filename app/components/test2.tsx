a7f1d1d785b53acf2d495a39b9013b72ab878545
Vasco Miguens <v_miguens@hotmail.com>
2023-08-27 15:15:36 +0100

    - created pages and addded them to the book - created page pivots and added them to the pivotSpine - page flipping animation based on scroll

--- app/components/test2.tsx
+++ app/components/test2.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const Book: React.FC = () => {
+  // reference to the container div
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -10,6 +11,7 @@ const Book: React.FC = () => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
     const renderer = new THREE.WebGLRenderer();
 
+    // Set the renderer size to match the window dimensions
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     // Add the renderer to the DOM
@@ -26,7 +28,7 @@ const Book: React.FC = () => {
     light.position.set(10, 10, 10);
     scene.add(light);
 
-    // Add shapes
+    // Define geometries, materials, and shapes for the book cover, spine, and pages
     const coverGeometry = new THREE.BoxGeometry(6.5, 8, 0.1);
     const pageGeometry = new THREE.BoxGeometry(6.4, 7.8, 0.2);
     const coverMaterial = new THREE.MeshLambertMaterial({ color: 0x2727e6 });
@@ -53,6 +55,25 @@ const Book: React.FC = () => {
     pivotSpine.add(spine);
     scene.add(pivotSpine);
 
+    //Pages
+    const pages: THREE.Object3D[] = [];
+    const pagePivots: THREE.Object3D[] = [];
+    const pageColors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff];
+    for (let i = 4; i >= 0; i--) {
+      const page = new THREE.Mesh(new THREE.BoxGeometry(6.4, 7.8, 0.2), new THREE.MeshLambertMaterial({ color: pageColors[i % pageColors.length] }));
+  
+      // Create a pivot for each page at the left edge
+      const pivotPage = new THREE.Object3D();
+      page.position.x = 3.15;
+      pivotPage.position.z = 0.1;
+      page.position.z = 0.01* (4 - i); // Adjust z-position calculation
+      pivotPage.add(page);
+      pages.push(page);
+      pagePivots.push(pivotPage); // Store the pivot
+
+    }
+    pivotSpine.add(...pagePivots);
+    
     // Second cover
     const cover2 = new THREE.Mesh(coverGeometry, coverMaterial);
     const pivot2 = new THREE.Object3D();
@@ -61,6 +82,7 @@ const Book: React.FC = () => {
     pivot2.add(cover2);
     scene.add(pivot2);
 
+    // Set initial camera position and rotation
     camera.position.z = 6;
     // camera.position.x = -3;
     // camera.position.y = -10;
@@ -69,22 +91,22 @@ const Book: React.FC = () => {
     // Animation
     const animate = function () {
       requestAnimationFrame(animate);
+        // Check if the container reference is available
       if (!containerRef.current) return;
 
+      
       const containerHeight = containerRef.current.clientHeight;
       const containerTop = containerRef.current.getBoundingClientRect().top;
       const scrollY = window.scrollY || window.pageYOffset;
 
       // Calculate scroll progress
       const progress = Math.min((scrollY + containerTop) / (containerHeight - window.innerHeight), 1);
+       // Update spine rotation and cover position based on scroll progress
       spine.rotation.y = Math.PI / 2;
-      // Calculate rotations
       const rotationYCover = Math.min(Math.PI * (progress / 0.1), Math.PI);
-      
-      // Cover rotation and position
       pivot.rotation.y = -rotationYCover;
       scene.position.x = rotationYCover * 3.25 / Math.PI;
-
+      // Update animations after the cover reaches 90 degrees
       if (rotationYCover >= Math.PI / 2 && rotationYCover <= Math.PI) {
         // Calculate the progress after the cover reaches 90 degrees
         const after90Progress = (rotationYCover - Math.PI / 2) / (Math.PI / 2);
@@ -97,8 +119,37 @@ const Book: React.FC = () => {
         const zPositionCover = 0.5 - 0.5 * after90Progress;
         pivot.position.z = zPositionCover;
 
-       
+        pagePivots.forEach((pivotPage, index) => {
+          pivotPage.rotation.y= after90Progress * Math.PI / 2;
+          pivotPage.position.z = 0.03 * index ;
+          pivotPage.position.x = 0.05 * after90Progress;
+        });
       }
+
+       // Update page flipping animations
+      if (progress > 0.12) {  
+        const pageProgress = (progress - 0.12) / 0.3;
+        pagePivots.forEach((page, index) => {
+          const reverseIndex = pages.length - index - 1;
+          const flipStart = reverseIndex / pages.length;
+          const flipEnd = (reverseIndex + 1) / pages.length;
+          
+          if (pageProgress >= flipStart && pageProgress <= flipEnd) {
+            const normalizedProgress = (pageProgress - flipStart) / (flipEnd - flipStart);
+            page.rotation.y = Math.PI / 2 - normalizedProgress * Math.PI; // Adjust the rotation width according to the final position
+            // Adjust z-position according to index of page
+            // page.position.z = 0.004 * index;
+            page.position.x = 0.05; // adjust x position to match with the rotation
+          } else if (pageProgress > flipEnd) {
+            page.rotation.y = -Math.PI / 2;
+            // Fix the z-position after the page has finished flipping
+            // page.position.z = 0.004 * index;
+            page.position.x = 0.02 ;
+          }
+        });
+        
+      
+    }
       renderer.render(scene, camera);
     };
     
